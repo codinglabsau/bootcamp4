efec978276286355f2f261ef8ee9ee1e35e1aa3e
Jay Modi <jay.modi105@gmail.com>
2020-03-17 13:14:15 +1000

    Minor changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel Project</title>
 
         <!-- FontAwesome -->
         <link href="{{asset('css/app.css')}}" rel="stylesheet">
