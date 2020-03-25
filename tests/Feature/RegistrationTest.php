b70a19a716ca4a493aeb0ad85a0bf8777cf882b2
Brendon Willoughby <Brendon.willoughby@gmail.com>
2020-03-26 10:42:02 +1100

    Split test into two with more accurate naming and clear the _variables.sass file.

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -13,16 +13,29 @@ class RegistrationTest extends TestCase
     use DatabaseMigrations;
     use RefreshDatabase;
     /**
-     * Tests that a user is registered and can access 
+     * Tests that a user is able to register with the website.
      *
      * @return void
      */
-    public function testAUserCanRegister()
+    public function testAUserIsSuccessfullyRegistered()
     {
         $this->seed('RoleTableSeeder');
-        $this->withoutExceptionHandling();
-        $user = factory(User::class)->create(['name' => 'John Smith', 'email' => 'student@email.com']);
 
+        $user = factory(User::class)->create(['name' => 'John Smith', 'email' => 'student@email.com']);
+        $lastEnteredUser = User::all()->last();
+
+        $this->assertNotNull($lastEnteredUser);
+        $this->assertEquals($user->name, $lastEnteredUser->name);
+        $this->assertEquals($user->email, $lastEnteredUser->email);
+    }
+
+    /**
+     * Tests that a registered user is able to access content.
+     */
+    public function testARegisteredUserCanAccessContent() {
+        $this->seed('RoleTableSeeder');
+
+        $user = factory(User::class)->create(['name' => 'John Smith', 'email' => 'student@email.com']);
         $response = $this->actingAs($user)->get('/home');
 
         $response->assertViewIs('home');
