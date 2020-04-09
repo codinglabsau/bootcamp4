38db374aeb3231d2c5ba2e0e55af32aff531bccb
stevethomas <steve@codinglabs.io>
2020-04-09 12:19:44 +1000

    proof of concept show course view

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Course;
 
 class CourseController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $courses = Course::all()->map(function ($course) {
             $course->description = explode('|', $course->description);
             return $course;
         });
+
         return view('bootcamp.index', ['courses' => $courses]);
+    }
+
+    public function show(Course $course)
+    {
+        return view('courses.show', compact('course'));
     }
 }
